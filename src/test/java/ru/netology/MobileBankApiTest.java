65407007422e327379c7305690ed5f8f984282a2
Aleksandr Kretov <kretov_a@outlook.com>
2022-04-27 12:31:41 +0300

    add not wokring tests , gitignore and jar file

---
+++ src/test/java/ru/netology/MobileBankApiTest.java
@@ -0,0 +1,64 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.*;
+
+
+
+class MobileBankApiTest {
+    @Test
+    void shouldReturnTwoHundred() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    void shouldReturnJson() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .contentType(ContentType.XML);
+    }
+
+    @Test
+    void shouldSatisfySchema() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+    @Test
+    void shouldHaveNotMinusAccounts() {
+
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[0].balance", greaterThanOrEqualTo(0))
+                .body("[1].balance", greaterThanOrEqualTo(0))
+                .body("[2].balance", greaterThanOrEqualTo(0))
+        ;
+    }
+}
